49938571a68c7bd07bc6d275404ca47912254226
Meysam Nosrati <meysamna514@gmail.com>
2021-04-03 18:08:32 +0430

    create base.html header & footer and extends to detail and home pages & set time django to Asia/Tehran

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-# from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse, JsonResponse, Http404
 from .models import Article
 
 # Create your views here.
@@ -11,6 +11,6 @@ def home(request):
 
 def detail(request, slug):
     context = {
-        "article": Article.objects.get(slug=slug)
+        "article": get_object_or_404(Article, slug=slug, status='p')
     }
-    return render(request, "blog/single.html", context)
+    return render(request, "blog/detail.html", context)
