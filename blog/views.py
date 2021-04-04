81b129c73481d49286d42c2e0f9b0d18fbbc1e55
Meysam Nosrati <meysamna514@gmail.com>
2021-04-04 18:07:11 +0430

    create templatetags: site-title and category_navbar

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,11 @@ from .models import Article, Category
 def home(request):
     context = {
         "articles": Article.objects.filter(status='p'),
-        "category": Category.objects.filter(status=True)
     }
     return render(request, "blog/home.html", context)
 
 def detail(request, slug):
     context = {
         "article": get_object_or_404(Article, slug=slug, status='p'),
-        "category": Category.objects.filter(status=True)
     }
     return render(request, "blog/detail.html", context)
