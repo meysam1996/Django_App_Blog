6e7f3a80fad687f5e59711c591f70e4d2a2c9708
Meysam Nosrati <meysamna514@gmail.com>
2021-04-04 15:04:13 +0430

    add Sahel Font and change body direction to rtl

--- blog/models.py
+++ blog/models.py
@@ -32,6 +32,9 @@ class Article(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
+    class Meta:
+        ordering = ['-publish']
+
     def __str__(self):
         return self.title
 
