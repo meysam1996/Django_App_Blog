ea8293e6d18d552624b20be51c692eafdb8a19e9
Meysam Nosrati <meysamna514@gmail.com>
2021-04-06 18:42:37 +0430

    I rewrote this views.py as a class base view

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,14 @@ class ArticleManager(models.Manager):
     def published(self):
         return self.filter(status='p')
 
+class CategoryManager(models.Manager):
+    def active(self):
+        return self.filter(status=True)
+
 
 # Create your models here.
 class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL, related_name='children', verbose_name='زیر دسته')
     title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس دسته بندی')
     status = models.BooleanField(default=True, verbose_name='آیا نمایش داده شود؟')
@@ -18,10 +23,14 @@ class Category(models.Model):
     class Meta:
         verbose_name = "دسته بندی"
         verbose_name_plural = "دسته بندی ها"
-        ordering = ['position']
+        ordering = ['parent__id', 'position']
 
     def __str__(self):
         return self.title
+
+    objects = CategoryManager()
+    
+
 
 class Article(models.Model):
     STATUS_CHOICES = [
